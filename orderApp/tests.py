a8d3b80f9bd8a3b0ddca7ea05aa6d578b7941e2a
hrharish111 <hrharish111@gmail.com>
2018-12-16 21:04:53 +0530

    adding testcase

--- orderApp/tests.py
+++ orderApp/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from decimal import Decimal
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class TestingOrders(APITestCase):
+    def testingOrderDetails(self):
+        self.url =reverse('order-list')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+class TestingItemList(APITestCase):
+    def testingDeleteItemList(self):
+        self.details_url = reverse('orderitems-detail',kwargs={'pk':3})
+        response = self.client.delete(self.details_url,{})
+        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
--- pizzaApp/tests.py
+++ pizzaApp/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from decimal import Decimal
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class TestingPizzaSize(APITestCase):
+    def testingCreatePizza(self):
+        self.url = reverse('pizzasize-list')
+        self.newsize = {'size':1000}
+        response = self.client.post(self.url,self.newsize)
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+
+class TestingPizzaApp(APITestCase):
+    def test_all_list_pizza(self):
+        self.url = reverse('pizza-list')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+class TestingPizzaVariationApp(APITestCase):
+    def testingUpdateVariation(self):
+        self.variation = {
+            'price':Decimal(12.00)
+        }
+        self.details = reverse('pizzavariation-detail',kwargs={'pk':1})
+        response = self.client.put(self.details,self.variation)
+        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
